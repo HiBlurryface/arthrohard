598aab3e230e32f01f8515bbfcfd5bd9413ab145
Alexandr <AlexandrKsenz@gmail.com>
2025-01-02 21:04:35 +0100

    fixed API request ( added pageSize & pageNum attr )

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let productsContainer = document.querySelector('#products');
 let productId = document.querySelector('#productId');
 let productText = document.querySelector('#productText');
 let productBtn = document.querySelector('.product__menu-btn');
+
 let productMenu = document.querySelector('.product__menu-list');
 
 let popup = document.querySelector('.popup');
@@ -19,29 +20,6 @@ burgerBtn.addEventListener('click', function () {
     burger.classList.toggle("active");
     body.classList.toggle('locked');
     burgerBg.classList.toggle('active');
-})
-
-productBtn.addEventListener('click', function () {
-    productMenu.classList.toggle("active");
-})
-
-function getProductItem(product) {
-    return `<div class="product__item" data-id="${product.id}" data-text="${product.text}">
-        <h4 class="product__item-title">ID:${product.id}</h4>
-    </div>`
-}
-
-function openProduct(product) {
-    productId.innerHTML = product.id;
-    productText.innerHTML = product.text;
-
-    body.classList.add('locked');
-    popup.classList.add('active');
-}
-
-popupClose.addEventListener('click', function () {
-    popup.classList.remove('active');
-    body.classList.remove('locked');
 })
 
 $('.header__menu-link').click(function () {
@@ -62,6 +40,27 @@ $(window).scroll(function () {
     })
 })
 
+// api scripts
+
+function getProductItem(product) {
+    return `<div class="product__item" data-id="${product.id}" data-text="${product.text}">
+        <h4 class="product__item-title">ID:${product.id}</h4>
+    </div>`
+}
+
+function openProduct(product) {
+    productId.innerHTML = product.id;
+    productText.innerHTML = product.text;
+
+    body.classList.add('locked');
+    popup.classList.add('active');
+}
+
+popupClose.addEventListener('click', function () {
+    popup.classList.remove('active');
+    body.classList.remove('locked');
+})
+
 window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
     const windowHeight = window.innerHeight;
@@ -72,8 +71,31 @@ window.addEventListener('scroll', () => {
     }
 });
 
+let pageSize = [10, 20, 30, 40, 50];
+let currentPageSize = 5;
+let currentPageNum = 1;
+
+function choosePageSize() {
+    $('.product__menu-btn').text(currentPageSize);
+}
+choosePageSize();
+productBtn.addEventListener('click', function () {
+    productMenu.classList.toggle("active");
+})
+
+let markup = '';
+$('.product__menu-item').click(function () {
+    currentPageSize = $(this).attr('data-val');
+    currentPageNum = 1;
+
+    choosePageSize();
+    getData();
+    markup = '';
+})
+
+
 async function getData() {
-    const url = "https://brandstestowy.smallhost.pl/api/random";
+    const url = `https://brandstestowy.smallhost.pl/api/random?pageNumber=${currentPageNum}&pageSize=${currentPageSize}`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -82,7 +104,6 @@ async function getData() {
         const json = await response.json();
 
         let data = json.data;
-        let markup = '';
 
         data.forEach((product) => {
             markup += getProductItem(product);
@@ -97,11 +118,11 @@ async function getData() {
                     id: event.target.dataset.id,
                     text: event.target.dataset.text
                 }
-
                 openProduct(data)
             })
         })
+
+        currentPageNum++;
     } catch (error) {
-        console.error(error.message);
     }
 }
